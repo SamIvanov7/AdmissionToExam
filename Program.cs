60e7d865015fe0adb0847fb658edf4957aee2f99
Sam Ivanov <si@juno-solar.com>
2023-11-05 15:02:12 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AdmissionToExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Enter grade " + (i + 1) + ": ");
+                double grade = double.Parse(Console.ReadLine());
+                sum += grade;
+            }
+
+            double average = sum / 5;
+            Console.WriteLine("Average grade: " + average);
+
+            if (average >= 4)
+            {
+                Console.WriteLine("The student is admitted to the exam.");
+            }
+            else
+            {
+                Console.WriteLine("The student is not admitted to the exam.");
+            }
+        }
+    }
+}
